cec3fe634f3eb963407a1e0e9133329ac697a88b
fabricioubatuba <bioranss@gmail.com>
2025-08-05 11:43:13 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,7 @@
-
+auth.onAuthStateChanged((user) => {
+    if (user && ['atendente@sejafibra.net', 'supervisor@sejafibra.net', 'bioranss@gmail.com'].includes(user.email)) {
+        if (typeof loadMaintenances === 'function') loadMaintenances();
+    } else {
+        window.location.href = 'login.html';
+    }
+});
